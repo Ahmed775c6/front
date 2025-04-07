b9ea867113c44e2fd00d9eb3e8c7e72305a5fdf5
unknown <ahmed.chouikh2020@gmail.com>
2025-04-07 06:07:40 +0200

    fix

--- src/Adminsator/Admin.tsx
+++ src/Adminsator/Admin.tsx
@@ -15,15 +15,16 @@ const Admin = () => {
 
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
   useEffect(() => {
+  
     document.body.classList.remove("light", "dark");
     document.body.classList.add(theme);
     localStorage.setItem("theme", theme);
     setTheme(theme)
-    if(token){
+    if(token && token != "null"){
       setLoading(false);
     }
 
-  }, [theme]);
+  }, [theme,token]);
 
 const [AsideT, setAside] = useState(false)
 
@@ -41,24 +42,26 @@ const [Changing,setChanging] = useState(false);
 {
   Loading ? <Loader/> : ''
 }
-{
-  Changing ? <Themes setCh = {setChanging} /> : ''
-}
-<div className="w-full flex dark:bg-[#2d3748] ">
-<Aside AsideT = {AsideT} setAsideT= {setAside}  />
-<div className="w-full min-h-[100vh] bg-[#edf4f6] dark:bg-[#2d3748] flex flex-col">
-        <Nav AsideT = {AsideT} setAside = {setAside} />
-        <MainDash />
-      </div>
-
-<button className="fixed justify-center right-10 bottom-10 rounded-full w-11 h-11 text-center items-center outline-none border-none cursor-pointer flex p-2 bg-blue-400 text-white"
-onClick={()=>{
-  setChanging(true);
-}}
->
-  <i className="ri-settings-line"></i>
-</button>
-</div>
+<>
+  {
+    Changing ? <Themes setCh = {setChanging} /> : ''
+  }
+  <div className="w-full flex dark:bg-[#2d3748] ">
+  <Aside AsideT = {AsideT} setAsideT= {setAside}  />
+  <div className="w-full min-h-[100vh] bg-[#edf4f6] dark:bg-[#2d3748] flex flex-col">
+          <Nav AsideT = {AsideT} setAside = {setAside} />
+          <MainDash />
+        </div>
+  
+  <button className="fixed justify-center right-10 bottom-10 rounded-full w-11 h-11 text-center items-center outline-none border-none cursor-pointer flex p-2 bg-blue-400 text-white"
+  onClick={()=>{
+    setChanging(true);
+  }}
+  >
+    <i className="ri-settings-line"></i>
+  </button>
+  </div>
+  </>
 </>
   )
 }
