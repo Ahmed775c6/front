88163dfe94bb6479f13129448cffffc3bb299fb1
unknown <ahmed.chouikh2020@gmail.com>
2025-05-05 07:30:51 +0200

    'uxxwqA'

--- src/pages/Makeup.tsx
+++ src/pages/Makeup.tsx
@@ -32,7 +32,7 @@ return (
 <Navbar />
 {
     Loading ? <ProductSkeleton /> : 
-    <div className="w-full grid grid-cols-3 gap-3 dkhdyh p-3 bh-white">
+    <div className="w-full grid grid-cols-3 gap-3 dkhdyh p-3 bg-white">
         {
             products.map((item:any) => (
            <div className="" key={item._id}>
