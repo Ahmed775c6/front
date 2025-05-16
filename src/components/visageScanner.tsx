731d5344ae263adc5b559ff9552ff2351e847a7a
unknown <ahmed.chouikh2020@gmail.com>
2025-05-16 02:23:31 +0200

    pe12

--- src/components/visageScanner.tsx
+++ src/components/visageScanner.tsx
@@ -1,5 +1,5 @@
 import { motion, useMotionValue } from "framer-motion";
-
+import { addToCart } from "../Logic/utils";
 import { useEffect, useRef, useState } from "react";
 type SliderProps = {
     images: any[];
@@ -7,9 +7,6 @@ type SliderProps = {
     height: number;
     reverse?: boolean;
   };
-  
-
-
   
   const Slider: React.FC<SliderProps> = ({ images, width, height, reverse }) => {
 
@@ -22,14 +19,36 @@ type SliderProps = {
           "--quantity": images.length,
         } as React.CSSProperties}
         data-reverse={reverse ? "true" : "false"}
+      
       >
         <div className="list">
           {images.map((src, index) => (
-            <div key={index} className="item border-none  relative" style={{ "--position": index + 1 } as React.CSSProperties}>
-<div className="w-full p-2 flex flex-col absolute top-0 left-0">
-    <p>{src.name}</p>
-    <span>{src.price}</span>
-</div>
+            <div key={index} className="item border-none relative cursor-pointer" style={{ "--position": index + 1 } as React.CSSProperties} onClick={()=>{window.location.href = `/ViewProduct?id=${src._id}`}} >
+ <div className="relative group">
+ {
+            src.discount > 0 ?   <span className="purcentage absolute top-5 left-2 z-20 bg-rose-100 text-rose-500 p-1 rounded-full text-xs font-medium">
+          ⚡  {src.discount} %
+          </span> : null
+          }
+      
+      
+  
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9, y: 10 }}
+        whileHover={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{ duration: 0.3, ease: "easeOut" }}
+        className="absolute top-36 right-0 w-full p-2 flex flex-col  bg-white shadow-xs rounded-none"
+      >
+        <p className="text-sm font-semibold text-gray-800 text-left">
+          {src.name}
+        </p>
+      <div className="w-full flex justify-between">
+      <span className="text-md font-medium  text-blue-700">{src.currentPrice} TND</span>
+      <i className=" bg-emerald-500 text-emerald-300 w-10 h-10 rounded-full float-end text-sm p-1  ri-shopping-cart-line cursor-pointer" onClick={()=>{addToCart(src)}}></i>
+      </div>
+      </motion.div>
+    </div>
+
               <img src={src.mainImage} alt="slider image "  />
       
             </div>
@@ -38,29 +57,28 @@ type SliderProps = {
       </div>
     );
   };
-    
+  
+  
   type MainProps = {
-    p: any;
+    pr: any;
   };
   
- 
-  const Main: React.FC<MainProps> = ({p} : any) => {
+  const Main: React.FC<MainProps> = ({ pr }) => {
+  
     return (
       <main>
    
         <Slider
-          images={p }
+          images={pr }
           width={200}
-          height={200}
+          height={400}
           
         />
-       <button className='p-2 bg-white w-full mt-auto rounded-3xl'>
-       Découvrez tout</button>
+        <button className='p-2 bg-white w-full rounded-3xl mt-auto'>
+        Découvrez tout</button>
       </main>
     );
   };
-  
-
   
 
 
@@ -99,7 +117,7 @@ const visageScanner = ({p} :any) => {
 <h1 className="text-[#144273] font-semibold text-[1.5rem]">Meilleures gammes pour sublimer votre peau par nos soins anti-taches brunes</h1>
 <p className="text-[1rem]">Adieu Taches Brunes !</p>
 <div className="w-full flex flex-col gap-3">
-<Main p= {p}/>
+<Main pr= {p}/>
 
 </div>
 </div>
@@ -109,7 +127,7 @@ const visageScanner = ({p} :any) => {
         key="before"
         src="./assets/before2.png"
         alt="before"
-       
+        loading="lazy"
         className="absolute inset-0 rounded-md p-2 w-full h-full object-cover"
         initial={{ opacity: 1 }}
         animate={{ opacity: showAfter ? 0 : 1 }}
@@ -121,7 +139,7 @@ const visageScanner = ({p} :any) => {
         key="after"
         src="./assets/after.png"
         alt="after"
-       
+        loading="lazy"
         className="absolute rounded-md inset-0 p-2 w-full h-full object-cover"
         initial={{ opacity: 0 }}
         animate={{ opacity: showAfter ? 1 : 0 }}
